0c3cda8c89c82859f7d777be5a2cbf6a122c5b03
RITH4NYA <rithanyasasikumar.18@gmail.com>
2025-04-24 21:53:49 +0530

    put request

--- Backend/models/MealPlan.js
+++ Backend/models/MealPlan.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const mealPlanSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   meals: [{
-    name: String,
+    name: {
+      type:String,
+      
+    },
     type: { type: String, enum: ['Breakfast', 'Lunch', 'Dinner', 'Snack', 'Post-Workout'] },
     ingredients: [String],
     calories: Number,
