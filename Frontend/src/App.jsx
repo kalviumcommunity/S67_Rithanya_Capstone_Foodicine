4aa5e338bd42f155bdf0a08a6d7051c17324cbd8
rithanya <rithanyasasikumar.18@mail.com>
2025-04-26 16:06:35 +0530

    frontend deployment

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import UserForm from './components/UserForm';
 
 function App() {
--- Frontend/src/components/UserForm.jsx
+++ Frontend/src/components/UserForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import api from '../api/axios';
 import './UserForm.css';
-
+import React from "react";
 
 function UserForm() {
   const [formData, setFormData] = useState({
